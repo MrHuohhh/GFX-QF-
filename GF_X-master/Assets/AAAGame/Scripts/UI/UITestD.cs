082904398557689a8512df2bbff573e9033bc5ea
CHINAMI-0TE8NQN\Administrator <370462450@QQ.com>
2024-10-31 11:49:27 +0800

    框架上传

---
+++ GF_X-master/Assets/AAAGame/Scripts/UI/UITestD.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using QFramework;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityGameFramework.Runtime;
+using Log = UnityGameFramework.Runtime.Log;
+
+public partial class UITestD : UIFormBase
+{
+    protected override void OnOpen(object userData)
+    {
+        base.OnOpen(userData);
+        var value = Params.Get<VarInt32>("Value");
+        Log.Info($"UITestD{value} Open with ");
+    }
+
+    protected override void OnButtonClick(object sender, Button btSelf)
+    {
+        base.OnButtonClick(sender, btSelf);
+        if (btSelf == varButton)
+        {
+            ActionKit.Sequence()
+                .Callback(() =>  Log.Error(">>>>>>>>>>>>>>1"))
+                .Callback(() =>  Log.Error(">>>>>>>>>>>>>>2"))
+                .Delay(0.3f)
+                .Callback(() => Log.Error(">>>>>>>>>>>>>>3"))
+                .Start(this);
+            Log.Error(">>>>>>>>>>>>>>Hello World");
+        }
+    }
+}
---
+++ GF_X-master/Assets/AAAGame/Scripts/UI/UIVariables/UITestD.Variables.cs
@@ -0,0 +1,13 @@
+﻿//---------------------------------
+//此文件由工具自动生成,请勿手动修改
+//更新自:CHINAMI-0TE8NQN
+//---------------------------------
+using UnityEngine;
+using UnityEngine.UI;
+public partial class UITestD
+{
+	[Space(10)]
+	[Header("UI Variables:")]
+	[SerializeField] private Image[] varLIne1Arr = null;
+	[SerializeField] private Button varButton = null;
+}
